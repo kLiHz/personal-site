3ca95a9443b0944f23ef12cf0327e57ee6b85274
henry_23 <henry.maoht@outlook.com>
2022-05-24 00:07:59 +0800

    Add CC BY-SA 4.0 License

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,6 +28,7 @@ const Layout = ({ location, title, children }) => {
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
+        . Licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>
         . See site <a href="https://github.com/tsagaanbar/blog/">source</a> on GitHub.
       </footer>
     </div>
